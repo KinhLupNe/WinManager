466ebd8484efdf095fb1f642f60b2ce513078916
KinhLupNe <kinhlupna@gmail.com>
2025-12-20 00:46:06 +0700

    Revert "Merge pull request #3 from KinhLupNe/feature/cpu-ui"
    This reverts commit eba3711f3f819c1c9202adbf45de559b43843b70, reversing
    changes made to 6bebfcef94463e5f8470b0428e1e91f6bbd33c90.

---
+++ WinManager/ViewModels/CpuPresenter.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Windows.Threading;
+using WinManager.Models;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+
+namespace WinManager.ViewModels
+{
+    public partial class CpuPresenter : ObservableObject
+    {
+        private readonly CpuModel _model = new CpuModel();
+        private readonly DispatcherTimer _timer;
+
+        [ObservableProperty]
+        private string usageText = "Đang đọc CPU...";
+
+        public CpuPresenter()
+        {
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            _timer.Tick += (s, e) => UpdateCpu();
+            _timer.Start();
+
+            UpdateCpu();
+        }
+
+        private void UpdateCpu()
+        {
+            float u = _model.GetCpuUsage();
+            UsageText = $" Đang sử dụng {u:0.0}%";
+        }
+    }
+}
